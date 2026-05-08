17201230f115e98fd81022d265ebca46e9857eaa
CustCoding <custodiotitossetitosse@gmail.com>
2026-05-09 01:39:35 +0200

    feat(models): define mempool data model

---
+++ atividade-1/backend/models/mempool.go
@@ -0,0 +1,10 @@
+package models
+
+type MempoolSummary struct {
+	TxCount         int            `json:"tx_count"`
+	TotalVSize      float64        `json:"total_vsize"`
+	AvgFeeRate      float64        `json:"avg_fee_rate"`
+	MinFeeRate      float64        `json:"min_fee_rate"`
+	MaxFeeRate      float64        `json:"max_fee_rate"`
+	FeeDistribution map[string]int `json:"fee_distribution"`
+}
