08ddbeaaa38a3584d3fac97e332b67456631b19d
CustCoding <custodiotitossetitosse@gmail.com>
2026-05-09 01:39:10 +0200

    feat(handlers): implement blockchain handler

---
+++ atividade-1/backend/handlers/blockchain_handler.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"corecraft-atividade-1/services"
+	"corecraft-atividade-1/rpc"
+)
+
+func BlockchainLagHandler(client *rpc.RPCClient) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		data, err := services.GetBlockchainLag(client)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, data)
+	}
+}
