0065804c5a142ed257175f45e9be4fc2f1749f5d
CustCoding <custodiotitossetitosse@gmail.com>
2026-05-11 23:39:06 +0200

    feat(atividade-3/zmq): add ZMQ subscriber

---
+++ atividade-3/internal/zmqsub/subscriber.go
@@ -0,0 +1,67 @@
+package zmqsub
+
+import (
+	"encoding/hex"
+	"log"
+	"time"
+
+	zmq "github.com/pebbe/zmq4"
+
+	"bitcoin-monitor-t3/internal/store"
+)
+
+func Start(zmqURL string, s *store.EventStore) {
+	if zmqURL == "" {
+		zmqURL = "tcp://127.0.0.1:28332"
+	}
+	log.Printf("[ZMQ] Conectando em %s", zmqURL)
+	for {
+		if err := run(zmqURL, s); err != nil {
+			log.Printf("[ZMQ] Erro: %v — reconectando em 5s...", err)
+			time.Sleep(5 * time.Second)
+		}
+	}
+}
+
+func run(zmqURL string, s *store.EventStore) error {
+	sock, err := zmq.NewSocket(zmq.SUB)
+	if err != nil {
+		return err
+	}
+	defer sock.Close()
+
+	sock.SetSubscribe("hashblock")
+	sock.SetSubscribe("hashtx")
+
+	if err := sock.Connect(zmqURL); err != nil {
+		return err
+	}
+	log.Println("[ZMQ] Subscrito em hashblock e hashtx")
+
+	for {
+		msg, err := sock.RecvMessageBytes(0)
+		if err != nil {
+			return err
+		}
+		if len(msg) < 2 {
+			continue
+		}
+		topic := string(msg[0])
+		hash := hex.EncodeToString(reverseBytes(msg[1]))
+		switch topic {
+		case "hashblock":
+			log.Printf("[ZMQ] 🟧 Bloco: %s", hash)
+			s.AddBlock(hash)
+		case "hashtx":
+			s.AddTx(hash)
+		}
+	}
+}
+
+func reverseBytes(b []byte) []byte {
+	out := make([]byte, len(b))
+	for i, v := range b {
+		out[len(b)-1-i] = v
+	}
+	return out
+}
